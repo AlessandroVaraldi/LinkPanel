b94511a78c1184ca4cef25e8c262c1bda3a82639
Alessandro <38320646+AlessandroVaraldi@users.noreply.github.com>
2023-11-11 16:43:05 +0100

    1.4.0
    - Buttons can be moved up or down
    - Code is more intuitive
    - Strings are stored in strings.xml

--- app/src/main/java/com/example/linkpanel/MainActivity.java
+++ app/src/main/java/com/example/linkpanel/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.LinearLayout;
 import android.widget.Button;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.content.res.ColorStateList;
@@ -23,10 +24,9 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    private static int NUM_BUTTONS = 1; // Numero dinamico di pulsanti
+    private int numButtons = 1; // Tracks the number of buttons dynamically
     private LinearLayout buttonContainer;
     private SharedPreferences prefs;
-
     public int maxStringLength = 100;
 
     @Override
@@ -40,67 +40,50 @@ public class MainActivity extends AppCompatActivity {
         buttonContainer = findViewById(R.id.buttonContainer);
 
         // Crea dinamicamente i pulsanti
-        NUM_BUTTONS = prefs.getInt("num_buttons", 1);
-        createButtons(NUM_BUTTONS);
+        numButtons = prefs.getInt("num_buttons", 1);
+        restoreButtons(numButtons);
     }
 
-    private void addButton() {
-        final int buttonIndex = NUM_BUTTONS;
-
-        String buttonText = "Pulsante " + (buttonIndex + 1);
-        String buttonUrl = "http://example.com";
-
-        // Salva i valori di default nelle SharedPreferences prima di aggiungere il pulsante alla UI
-        saveButtonData(buttonIndex, true, buttonText);
-        saveButtonData(buttonIndex, false, buttonUrl);
-
-        Button newButton = configureButton(buttonText);
-        newButton.setTag(buttonIndex);
-
-        newButton.setOnClickListener(v -> openWebPage(buttonUrl));
-
-        // Configura i listener specifici per il nuovo pulsante, se necessario.
-        newButton.setOnLongClickListener(v -> {
-            showPopupMenu(v, buttonIndex);
-            return true;
-        });
-
-        // Aggiungi il nuovo pulsante al contenitore
-        buttonContainer.addView(newButton);
-
-        // Incrementa il conteggio dei pulsanti e aggiorna le SharedPreferences
-        NUM_BUTTONS++;
-        saveButtonCount();
-    }
-
-    private void saveButtonCount() {
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putInt("num_buttons", NUM_BUTTONS);
-        editor.apply();
-    }
-
-    private void createButtons(int numButtons) {
+    private void restoreButtons(int numButtons) {
+        // Restore custom buttons on startup
         for (int i = 0; i < numButtons; i++) {
-            final int index = i;
             String buttonTextKey = "button_text_" + i;
             String buttonUrlKey = "button_url_" + i;
             String buttonText = prefs.getString(buttonTextKey, "Pulsante " + (i + 1));
             String buttonUrl = prefs.getString(buttonUrlKey, "http://example.com");
-
-            Button button = configureButton(buttonText);
-            button.setTag(i);
-
-            button.setOnClickListener(v -> openWebPage(buttonUrl));
-            button.setOnLongClickListener(v -> {
-                showPopupMenu(v, index);
-                return true;
-            });
-
-            buttonContainer.addView(button);
+            addButtonUI(buttonText, buttonUrl, i);
         }
     }
 
+    private void createButton() {
+        // Create a button...
+        String buttonText = getString(R.string.button) + (numButtons + 1);
+        String buttonUrl = getString(R.string.exampleDomain);
+        addButtonUI(buttonText, buttonUrl, numButtons);
+        numButtons++;
+    }
+
+    private void addButtonUI(String buttonText, String buttonUrl, int index) {
+        // Add the created button to the UI...
+        Button button = configureButton(buttonText);
+        button.setTag(index);
+
+        button.setOnClickListener(v -> openWebPage(buttonUrl));
+        button.setOnLongClickListener(v -> {
+            showPopupMenu(v, index);
+            return true;
+        });
+
+        buttonContainer.addView(button);
+
+        // Save data and count after adding a button
+        saveButtonData(index, true, buttonText);
+        saveButtonData(index, false, buttonUrl);
+        saveButtonCount();
+    }
+
     private Button configureButton(String buttonText) {
+        // Configuration of button appearance...
         Button button = new Button(this);
         button.setId(View.generateViewId());
         button.setBackgroundTintList(ColorStateList.valueOf(0xFF1E4918));
@@ -120,18 +103,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openWebPage(String url) {
+        // Opening a web page...
         Uri webpage = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         intent.addCategory(Intent.CATEGORY_BROWSABLE);
-        Intent chooser = Intent.createChooser(intent, "Apri con");
+        Intent chooser = Intent.createChooser(intent, getString(R.string.openWith));
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(chooser);
         } else {
-            Toast.makeText(this, "Nessuna applicazione disponibile per aprire il link", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.noAppAvailable, Toast.LENGTH_LONG).show();
         }
     }
 
     private void showPopupMenu(View view, int buttonIndex) {
+        // Popup menu for editing/deleting a button...
         PopupMenu popupMenu = new PopupMenu(this, view);
         popupMenu.inflate(R.menu.popup_menu);
         popupMenu.setOnMenuItemClickListener(item -> {
@@ -142,57 +127,122 @@ public class MainActivity extends AppCompatActivity {
                 showEditDialog(buttonIndex, false);
                 return true;
             } else if (item.getItemId() == R.id.delete_button){
-                if (NUM_BUTTONS == 1){
-                    Toast.makeText(this, "Non puoi avere zero pulsanti!", Toast.LENGTH_LONG).show();
+                if (numButtons == 1){
+                    Toast.makeText(this, R.string.noZeroButtons, Toast.LENGTH_LONG).show();
                     clearAllButtons();
-                    addButton();
                 } else deleteButton(buttonIndex);
                 return true;
-            }else return false;
+            } else if (item.getItemId() == R.id.move_up){
+                moveButton(buttonIndex, true);
+                return true;
+            } else if (item.getItemId() == R.id.move_down){
+                moveButton(buttonIndex, false);
+                return true;
+            } else return false;
         });
         popupMenu.show();
     }
 
+    private void moveButton(int buttonIndex, boolean moveUp) {
+        if (moveUp && buttonIndex == 0) {
+            Toast.makeText(this, R.string.cannotMoveUp, Toast.LENGTH_SHORT).show();
+            return;
+        } else if (!moveUp && buttonIndex == numButtons - 1) {
+            Toast.makeText(this, R.string.cannotMoveDown, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int swapIndex = moveUp ? buttonIndex - 1 : buttonIndex + 1;
+
+        // Swap in SharedPreferences
+        swapButtonDataInPrefs(buttonIndex, swapIndex);
+
+        // Update UI
+        Button currentButton = findButtonByIndex(buttonIndex);
+        Button swapButton = findButtonByIndex(swapIndex);
+
+        if (currentButton != null && swapButton != null) {
+            // Swap positions in the LinearLayout
+            buttonContainer.removeView(currentButton);
+            buttonContainer.removeView(swapButton);
+
+            if (moveUp) {
+                buttonContainer.addView(currentButton, swapIndex);
+                buttonContainer.addView(swapButton, buttonIndex);
+            } else {
+                buttonContainer.addView(swapButton, buttonIndex);
+                buttonContainer.addView(currentButton, swapIndex);
+            }
+
+            // Update tags
+            currentButton.setTag(swapIndex);
+            swapButton.setTag(buttonIndex);
+        }
+
+        // Update button click and long click listeners if necessary
+        updateButtonListeners();
+    }
+
+    private void swapButtonDataInPrefs(int buttonIndex, int swapIndex) {
+        SharedPreferences.Editor editor = prefs.edit();
+
+        String currentButtonText = prefs.getString("button_text_" + buttonIndex, "");
+        String currentButtonUrl = prefs.getString("button_url_" + buttonIndex, "");
+        String swapButtonText = prefs.getString("button_text_" + swapIndex, "");
+        String swapButtonUrl = prefs.getString("button_url_" + swapIndex, "");
+
+        editor.putString("button_text_" + buttonIndex, swapButtonText);
+        editor.putString("button_url_" + buttonIndex, swapButtonUrl);
+        editor.putString("button_text_" + swapIndex, currentButtonText);
+        editor.putString("button_url_" + swapIndex, currentButtonUrl);
+
+        editor.apply();
+    }
+
+    private void updateButtonListeners() {
+        for (int i = 0; i < buttonContainer.getChildCount(); i++) {
+            Button button = (Button) buttonContainer.getChildAt(i);
+            int buttonIndex = (int) button.getTag();
+
+            button.setOnClickListener(v -> openWebPage(prefs.getString("button_url_" + buttonIndex, "")));
+            button.setOnLongClickListener(v -> {
+                showPopupMenu(v, buttonIndex);
+                return true;
+            });
+        }
+    }
+
     private void deleteButton(int buttonIndex) {
-        // Trova e rimuovi il pulsante dalla UI
+        // Deleting a button...
         Button buttonToRemove = findButtonByIndex(buttonIndex);
         if (buttonToRemove != null) {
             buttonContainer.removeView(buttonToRemove);
         }
 
-        // Decrementa il conteggio dei pulsanti
-        NUM_BUTTONS--;
+        numButtons--;
 
-        // Sposta in avanti le SharedPreferences dei pulsanti rimanenti
         SharedPreferences.Editor editor = prefs.edit();
-        for (int i = buttonIndex; i < NUM_BUTTONS; i++) {
+        for (int i = buttonIndex; i < numButtons; i++) {
             String buttonText = prefs.getString("button_text_" + (i + 1), null);
             String buttonUrl = prefs.getString("button_url_" + (i + 1), null);
             editor.putString("button_text_" + i, buttonText);
             editor.putString("button_url_" + i, buttonUrl);
         }
 
-        // Rimuovi le SharedPreferences dell'ultimo pulsante che ora è superfluo
-        editor.remove("button_text_" + NUM_BUTTONS);
-        editor.remove("button_url_" + NUM_BUTTONS);
-
-        // Applica i cambiamenti alle SharedPreferences
+        editor.remove("button_text_" + numButtons);
+        editor.remove("button_url_" + numButtons);
         editor.apply();
 
-        // Aggiorna il tag e il listener di click dei pulsanti rimanenti
         updateButtonsAfterDeletion();
-
-        // Salva il nuovo conteggio dei pulsanti
         saveButtonCount();
     }
 
     private void updateButtonsAfterDeletion() {
-        // Aggiorna il tag e i listener di click di tutti i pulsanti
+        // Updating buttons after deletion...
         for (int i = 0; i < buttonContainer.getChildCount(); i++) {
             Button button = (Button) buttonContainer.getChildAt(i);
-            button.setTag(i); // Riassegna gli indici
+            button.setTag(i);
 
-            // Aggiorna il listener di click con l'URL corretto dalle SharedPreferences
             int finalI = i;
             button.setOnClickListener(v -> {
                 String buttonUrl = prefs.getString("button_url_" + finalI, "");
@@ -204,15 +254,16 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             });
 
-            // Opzionalmente, aggiorna il testo del pulsante se necessario
             String buttonText = prefs.getString("button_text_" + i, "");
             button.setText(buttonText);
         }
+        saveButtonCount();
     }
 
     private void showEditDialog(int buttonIndex, boolean isText) {
+        // Showing edit dialog...
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(isText ? "Modifica Testo" : "Modifica URL");
+        builder.setTitle(isText ? getString(R.string.editText) : getString(R.string.editUrl));
 
         final EditText input = new EditText(this);
         input.setInputType(isText ? InputType.TYPE_CLASS_TEXT : InputType.TYPE_TEXT_VARIATION_URI);
@@ -220,21 +271,20 @@ public class MainActivity extends AppCompatActivity {
         input.setText(currentData);
         builder.setView(input);
 
-        builder.setPositiveButton("Salva", (dialog, which) -> {
+        builder.setPositiveButton(R.string.save, (dialog, which) -> {
             String newData = input.getText().toString();
 
             if (isText && newData.length() > maxStringLength) {
-                Toast.makeText(getApplicationContext(), "La stringa è troppo lunga", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), R.string.stringTooLong, Toast.LENGTH_SHORT).show();
             } else if (!isText && !isValidUrl(newData)) {
-                Toast.makeText(getApplicationContext(), "L'URL inserito non è valido", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), R.string.invalidUrl, Toast.LENGTH_SHORT).show();
             } else {
                 saveButtonData(buttonIndex, isText, newData);
                 updateButton(buttonIndex, isText, newData);
             }
         });
 
-        builder.setNegativeButton("Annulla", (dialog, which) -> dialog.cancel());
-
+        builder.setNegativeButton(R.string.cancel, (dialog, which) -> dialog.cancel());
         builder.show();
     }
 
@@ -248,23 +298,28 @@ public class MainActivity extends AppCompatActivity {
                             !parsedUrl.getProtocol().equalsIgnoreCase("https"))) {
                 return false;
             }
-            if (parsedUrl.getHost() == null) {
-                throw new MalformedURLException("L'URL non ha un host valido.");
+
+            if (parsedUrl.getHost() == null || parsedUrl.getHost().isEmpty()) {
+                return false;
             }
 
-            return true;
+            return url.matches(getString(R.string.allowedUrlCharacters));
+
         } catch (MalformedURLException | URISyntaxException e) {
+            Log.e("MainActivity", "Invalid URL: " + e.getMessage());
             return false;
         }
     }
 
     private void saveButtonData(int buttonIndex, boolean isText, String data) {
+        // Saving button data...
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString(isText ? "button_text_" + buttonIndex : "button_url_" + buttonIndex, data);
         editor.apply();
     }
 
     private void updateButton(int buttonIndex, boolean isText, String newData) {
+        // Updating a button...
         Button buttonToUpdate = findButtonByIndex(buttonIndex);
 
         if (buttonToUpdate != null) {
@@ -278,6 +333,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Button findButtonByIndex(int buttonIndex) {
+        // Finding a button by index...
         for (int i = 0; i < buttonContainer.getChildCount(); i++) {
             Button button = (Button) buttonContainer.getChildAt(i);
             if (button.getTag() != null && button.getTag() instanceof Integer && (int) button.getTag() == buttonIndex) {
@@ -288,47 +344,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Options menu...
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Gestisci le voci del menu dell'ActionBar qui.
+        // Handling item selection...
         int id = item.getItemId();
 
         if (id == R.id.action_clear_all) {
             showClearConfirmationDialog();
         } else if (id == R.id.action_add_button) {
-            if(NUM_BUTTONS < 25){
-                addButton();
+            if(numButtons < 25){
+                createButton();
             } else {
-                Toast.makeText(getApplicationContext(), "Massimo numero di pulsanti raggiunto!", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), R.string.maxButtonsAlert, Toast.LENGTH_LONG).show();
             }
-
         }
         return super.onOptionsItemSelected(item);
     }
 
     private void showClearConfirmationDialog() {
         new AlertDialog.Builder(this)
-                .setTitle("Conferma Rimozione")
-                .setMessage("Sei sicuro di voler rimuovere tutti i pulsanti?")
+                .setTitle(R.string.clearTitle)
+                .setMessage(R.string.clearQuestion)
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .setPositiveButton(android.R.string.yes, (dialog, whichButton) -> clearAllButtons())
                 .setNegativeButton(android.R.string.no, null).show();
     }
 
     private void clearAllButtons() {
-        // Rimuove tutti i pulsanti dalla LinearLayout
+        // Clearing all buttons..
         buttonContainer.removeAllViews();
 
-        // Reimposta il conteggio dei pulsanti a 0 e cancella le SharedPreferences
-        NUM_BUTTONS = 0;
+        numButtons = 0;
         SharedPreferences.Editor editor = prefs.edit();
-        editor.clear(); // Questo cancella TUTTE le SharedPreferences. Usa con cautela!
-        editor.putInt("num_buttons", NUM_BUTTONS); // Salva il nuovo conteggio dei pulsanti
+        editor.clear();
+        editor.putInt("num_buttons", numButtons);
         editor.apply();
-        addButton();
+        createButton(); // Create a button so UI is not empty
+    }
+
+    private void saveButtonCount() {
+        // Saves the current button count to SharedPreferences...
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("num_buttons", numButtons);
+        editor.apply();
     }
 }
